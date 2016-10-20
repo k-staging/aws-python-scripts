01d2baf605c267d295eff40cccf633b8a007db96
k-staging <dorreddo39@gmail.com>
2016-10-20 13:19:20 +0900

    ec2_describe.py修正

--- ec2_describe.py
+++ ec2_describe.py
@@ -10,18 +10,17 @@ def ec2_describe(profile):
     ec2_list = ec2_list.describe_instances()
     ec2_list = ec2_list['Reservations']
     for ec2 in ec2_list:
+        instance_nametag = ec2['Instances'][0]['Tags'][0]['Value']
         instance_id = ec2['Instances'][0]['InstanceId']
-        publicdns_name = ec2['Instances'][0]['PublicDnsName']
-        instance_launchtime = ec2['Instances'][0]['LaunchTime']
+        public_dns = ec2['Instances'][0]['PublicDnsName']
+        private_ip = ec2['Instances'][0]['PrivateIpAddress']
         instance_type = ec2['Instances'][0]['InstanceType']
-        instance_nametag = ec2['Instances'][0]['Tags']
-        print '{0} \t {1} \t {2} \t {3}'.format( instance_id, instance_type, instance_launchtime, publicdns_name )
+        instance_launchtime = ec2['Instances'][0]['LaunchTime']
+        print '{0: <25}{1: <15}{2: <15}{3: <60}{4: <15}{5}'.format( instance_nametag, instance_id, instance_type, public_dns, private_ip, instance_launchtime )
 
-# main
 if __name__ == '__main__':
     argvs = sys.argv
     argc = len(argvs)
-
     if (argc != 2):
         print 'example: python ec2_describe.py [profile_name]'
         quit()
