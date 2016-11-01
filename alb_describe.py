3eb9c4acfc07187764b58a46f1117936311d7ac4
k-staging <k-staging@users.noreply.github.com>
2016-11-01 14:55:58 +0900

    Schemeを追加

--- alb_describe.py
+++ alb_describe.py
@@ -12,10 +12,10 @@ def alb_describe(profile):
     for alb in alb:
         alb_arn = alb['LoadBalancerArn'].replace("/"," ")
         alb_arn = alb_arn.split(' ')
+        scheme = alb['Scheme']
         alb_dnsname = alb['DNSName']
-        print '{0}/{1} \t {2}'.format( alb_arn[-2], alb_arn[-1], alb_dnsname )
+        print '{0}/{1: <30} {2: <25} {3}'.format( alb_arn[-2], alb_arn[-1], scheme, alb_dnsname )
 
-# main
 if __name__ == '__main__':
     argvs = sys.argv
     argc = len(argvs)
@@ -23,6 +23,6 @@ if __name__ == '__main__':
     if (argc != 2):
         print 'example: python alb_describe.py [profile_name]'
         quit()
-
     profile = argvs[1]
+
     alb_describe(profile)
