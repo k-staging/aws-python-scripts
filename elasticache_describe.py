32681a63f90a18de30e5a822e90b8916688f6366
k-staging <k-staging@users.noreply.github.com>
2016-11-01 14:58:20 +0900

    PG追加

--- elasticache_describe.py
+++ elasticache_describe.py
@@ -12,20 +12,20 @@ def elasticache_describe(profile):
     for page in page_iterator:
         for elasticache in page['CacheClusters']:
             clusterid = elasticache['CacheClusterId']
-            replicaid = ''
-            if 'ReplicationGroupId' in elasticache:
-                replicaid = elasticache['ReplicationGroupId']
             instance_type = elasticache['CacheNodeType']
             engine = elasticache['Engine']
             engine_ver = elasticache['EngineVersion']
-            maintenance_window= elasticache['PreferredMaintenanceWindow']
+            pg = elasticache['CacheParameterGroup']['CacheParameterGroupName']
+            replicaid = ''
+            if 'ReplicationGroupId' in elasticache:
+                replicaid = elasticache['ReplicationGroupId']
             for elasticache in elasticache['CacheNodes']:
                 endpoint = elasticache['Endpoint']['Address']
                 az = elasticache['CustomerAvailabilityZone']
                 create_time = elasticache['CacheNodeCreateTime']
-                print '{0: <20} {1: <17} {2: <60} {3}{4: <7} {5: <12} {6: %Y-%m-%d-%H:%M}  {7}'.format(
+                print '{0: <20} {1: <17} {2: <60} {3}{4: <7} {5: <12} {6: %Y-%m-%d-%H:%M} {7: <17}  {8}'.format(
                     clusterid, instance_type, endpoint, engine,
-                    engine_ver, az, create_time, replicaid
+                    engine_ver, az, create_time, pg, replicaid
                 )
 
 if __name__ == '__main__':
@@ -34,6 +34,6 @@ if __name__ == '__main__':
     if argc != 2:
         print 'python ec2_describe.py [profile]'
         quit()
-
     profile = argvs[1]
+
     elasticache_describe(profile)
